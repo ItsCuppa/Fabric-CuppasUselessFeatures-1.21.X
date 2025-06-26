6c05842069c87dbe14e770eeb2a220c40379451d
Cuppa <cuppathings@gmail.com>
2025-06-26 23:10:22 +0100

    Changed entire registry script

---
+++ src/main/java/net/cuppa/cuppasuselessfeatures/item/ModItems.java
@@ -0,0 +1,49 @@
+package net.cuppa.cuppasuselessfeatures.item;
+
+import net.cuppa.cuppasuselessfeatures.CuppasUselessFeatures;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.MELON_TEST));
+    }
+
+    public static final Item MELON_TEST = register("melon_test", Item::new, new Item.Settings());
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CuppasUselessFeatures.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(CuppasUselessFeatures.MOD_ID, name), item);
+    }
+
+    public static void registeredModItems(){
+        CuppasUselessFeatures.LOGGER.info("Registering Mod Items for " + CuppasUselessFeatures.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(MELON_TEST);
+        });
+    }
+}
